526dedd77a1d8848066559225a1ec4480e63201a
Ruhong Lin <huntingkevin89@gmail.com>
2019-03-19 00:51:42 +0800

    move methods

--- src/main/java/com/ruhong/refactoring/chapter1/Rental.java
+++ src/main/java/com/ruhong/refactoring/chapter1/Rental.java
@@ -30,31 +30,10 @@ public class Rental {
     }
 
     public double getCharge() {
-        double result = 0;
-        switch (get_movie().get_priceCode()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (get_daysRented() > 2) {
-                    result += (get_daysRented() - 2) * 1.5;
-                }
-                break;
-            case Movie.NEW_RELEASE:
-                result += get_daysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (get_daysRented() > 3) {
-                    result += (get_daysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return result;
+        return _movie.getCharge(_daysRented);
     }
 
     public int getFrequentRenterPoints() {
-        if ((get_movie().get_priceCode() == Movie.NEW_RELEASE) && get_daysRented() > 1) {
-            return 2;
-        }
-        return 1;
+        return _movie.getFrequentRenterPoints(_daysRented);
     }
 }
