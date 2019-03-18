97187102aceacca7940e2c6bb6be508feac2ed69
Ruhong Lin <huntingkevin89@gmail.com>
2019-03-19 00:34:58 +0800

    replace temp with query

--- src/main/java/com/ruhong/refactoring/chapter1/Customer.java
+++ src/main/java/com/ruhong/refactoring/chapter1/Customer.java
@@ -26,7 +26,6 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
         int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
@@ -35,11 +34,20 @@ public class Customer {
 
             frequentRenterPoints += each.getFrequentRenterPoints();
             result += "\t" + each.get_movie().get_title() + "\t" + String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
 
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        return result;
+    }
+
+    private double getTotalCharge() {
+        double result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getCharge();
+        }
         return result;
     }
 
